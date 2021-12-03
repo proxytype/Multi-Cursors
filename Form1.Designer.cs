3df0c56c863b340a8631b88e45005bfe6e53bd48
proxytype <29818286+proxytype@users.noreply.github.com>
2021-12-03 09:11:21 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace MultiCursors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sec_cursor = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sec_cursor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sec_cursor
+            // 
+            this.sec_cursor.Location = new System.Drawing.Point(119, 105);
+            this.sec_cursor.Name = "sec_cursor";
+            this.sec_cursor.Size = new System.Drawing.Size(47, 50);
+            this.sec_cursor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.sec_cursor.TabIndex = 2;
+            this.sec_cursor.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.sec_cursor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveCaption;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sec_cursor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox sec_cursor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiCursors
+{
+    public partial class Form1 : Form
+    {
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public int X;
+            public int Y;
+        };
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        public static Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Int32 vKey);
+
+        MemoryStream cursor;
+        Image cursorImage;
+
+        int cursorWidth = 0;
+        int cursorHeight = 0;
+
+        Thread t;
+
+        public Form1()
+        {
+            cursorImage = Image.FromFile(@"cursor1.png");
+            this.WindowState = FormWindowState.Maximized;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.Focus();
+            t = new Thread(() =>
+            {
+                bool test = false;
+                while (true)
+                {
+
+                    try
+                    {
+                        DrawCursorsOnForm(GetMousePosition());
+
+                        bool mousePress = (GetAsyncKeyState(0x01) & (1 << 15)) != 0;
+                        if (mousePress)
+                        {
+                            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, sec_cursor.Location.X, sec_cursor.Location.Y, 0, 0);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                       
+                    }
+        
+
+                    Thread.Sleep(10);
+                }
+            });
+
+            t.Start();
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                // Set the form click-through
+                cp.ExStyle |= 0x80000 /* WS_EX_LAYERED */ | 0x20 /* WS_EX_TRANSPARENT */;
+                return cp;
+            }
+        }
+
+        private MemoryStream getStream(string file)
+        {
+
+            byte[] f = File.ReadAllBytes(file);
+            MemoryStream stream = new MemoryStream(f);
+            return stream;
+        }
+
+        private void DrawCursorsOnForm(Point p)
+        {
+            p.X = (int)(p.X / 1.1);
+
+            MethodInvoker invoker = delegate
+            {
+
+                if (sec_cursor.Width == 0 || sec_cursor.Height == 0)
+                {
+                    this.sec_cursor.Width = this.cursorWidth;
+                    this.sec_cursor.Height = this.cursorHeight;
+                }
+
+                sec_cursor.Location = p;
+
+                if (sec_cursor.Image != null)
+                {
+                    sec_cursor.Image.Dispose();
+                }
+
+                sec_cursor.Image = (Image)cursorImage.Clone();
+            };
+
+            this?.Invoke(invoker);
+
+
+        }
+
+
+     
+     
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (t != null && t.IsAlive) {
+                t.Abort();
+            }
+        }
+    }
+}
